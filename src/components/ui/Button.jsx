4f4a174589a894c7a2a70b0a5e3d6be65c611dc7
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-11-11 22:01:23 +0200

    added a button component

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,39 @@
+const Button = ({
+	children,
+	Icon,
+	onClick,
+	disabled = false,
+	IconClassName,
+	iconSize = 20,
+	variant = 'primary',
+	size = 'md',
+	className = '',
+}) => {
+	const variants = {
+		primary: 'bg-blue-600 text-white hover:bg-blue-700',
+		secondary:
+			'bg-transparent border-2 border-blue-600 text-blue-600 hover:bg-blue-50 dark:hover:bg-slate-700',
+		danger: 'bg-red-600 text-white hover:bg-red-700',
+	};
+
+	const sizes = {
+		sm: 'px-3 py-1.5 text-sm',
+		md: 'px-4 py-2 text-base',
+		lg: 'px-6 py-3 text-lg',
+	};
+
+	return (
+		<button
+			onClick={disabled ? undefined : onClick}
+			className={`rounded-xl font-semibold transition-all hover:scale-105 active:scale-95 flex items-center gap-2 ${
+				variants[variant]
+			} ${sizes[size]} ${
+				disabled ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'
+			} `}
+		>
+			{Icon && <Icon className={IconClassName} size={iconSize} />} {children}
+		</button>
+	);
+};
+
+export default Button;
