e33624063f178c2e46739daa5722e519a43b0846
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-11-11 19:52:04 +0200

    edited app and css page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 
+import DashboardPage from './pages/Dashboard';
 import CalenderPage from './pages/Calendar';
 import CurrencyPage from './pages/Currency';
-import DashboardPage from './pages/Dashboard';
 import HoroscopePage from './pages/Horoscope';
 import MoodPayerPage from './pages/MoodPlayer';
 import NewsPage from './pages/News';
@@ -17,18 +17,18 @@ import PageNotFound from './pages/PageNotFound';
 const App = () => {
 	return (
 		<Routes>
+			<Route path="/" element={<DashboardPage />} />
 			<Route path="/about" element={<AboutPage />} />
 			<Route path="/calendar" element={<CalenderPage />} />
 			<Route path="/currency" element={<CurrencyPage />} />
-			<Route path="/dashboard" element={<DashboardPage />} />
 			<Route path="/horoscope" element={<HoroscopePage />} />
 			<Route path="/moodPlayer" element={<MoodPayerPage />} />
 			<Route path="/news" element={<NewsPage />} />
 			<Route path="/quotes" element={<QuotePage />} />
 			<Route path="/setting" element={<SettingsPage />} />
 			<Route path="/todoList" element={<TodoListPage />} />
-			<Route path="weather" element={<WeatherPage />} />
-			<Route path="worldClock" element={<WorldClockPage />} />
+			<Route path="/weather" element={<WeatherPage />} />
+			<Route path="/worldClock" element={<WorldClockPage />} />
 			<Route path="*" element={<PageNotFound />} />
 		</Routes>
 	);
